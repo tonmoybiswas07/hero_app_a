ddb5b616ad1374e7f894a73302373ff5fd6dd961
tonmoybiswas9 <tonmoy.b3399@gmail.com>
2025-10-08 01:57:23 +0600

    added initial project setup

---
+++ src/Pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h3>data not found</h3>
+    </div>
+  );
+};
+
+export default Error;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Roots/Root";
+import Error from "../Pages/Error/Error";
+import Home from "../Pages/Home/Home";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
+  },
+]);
